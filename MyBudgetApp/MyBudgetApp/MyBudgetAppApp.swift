6a3a257ca54536d6f60c770d3d522ef01107bf1a
Dias <59363378+DDiasPTW@users.noreply.github.com>
2023-09-26 16:11:37 +0100

    Initial commit

---
+++ MyBudgetApp/MyBudgetApp/MyBudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetAppApp.swift
+//  MyBudgetApp
+//
+//  Created by Diogo Dias on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
